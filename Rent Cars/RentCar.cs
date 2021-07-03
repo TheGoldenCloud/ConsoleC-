982f6f5a23383e0059b8b6ae63c174cf5475be86
Vladimir Jovanovic <44228970+TheGoldenCloud@users.noreply.github.com>
2021-07-03 19:57:56 +0200

    Update RentCar.cs

--- Rent Cars/RentCar.cs
+++ Rent Cars/RentCar.cs
@@ -1,11 +1,9 @@
 
 class RentCar : Car{
-    public bool stanjeDostupnosti { get; set; }   //Koristicu kasnije
     public double pricePerDay { get; set; }
     
     public RentCar(){}
     public RentCar(string model, string mark, string color, int yearMade, int mileage,double pricePerDay) : base(model,mark,color,yearMade,mileage){
-        this.stanjeDostupnosti = stanjeDostupnosti;
         this.pricePerDay = pricePerDay;
     }
     public override string ToString()
